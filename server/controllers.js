d88369a773b78d9ab2b4de3d7c4b56ca0bb97c17
Caroline Peake <peake.caroline@gmail.com>
2022-08-08 09:04:48 -0700

    delete as many comments as I can befoe august 8, 9am for code review after 1) debugging/re-writing app so postman returns successful response, 2) working with helpdesk to fix issues pushing to github, 3) unsuccessfully  working with helpdesk to locate lost files/commits with final SQL queries

--- server/controllers.js
+++ server/controllers.js
@@ -1,6 +1,7 @@
-const db  = require('../db/postgres/index.js');
+//is it better to break connection & credentials into separate module (i.e, ./db/postgres/index.js)?
+
 const models = require('../db/postgres/models.js');
-const express = require('express');
+const express = require('express'); // test - pretty sure I don't need this
 
 // export.getAllProducts = async (req, res, next) => {
 //   models.queryProducts async (query, err, (err, result) => {
@@ -45,8 +46,6 @@ const express = require('express');
 module.exports = {
 
   getProduct: function (req, res) {
-    // console.log('req: ', req);
-    // console.log('query id: ', req.params);
     const { product_id } = req.params;
     return models.get_Product(product_id)
       .then(result => res.status(200).send(result.rows[0]))
@@ -56,152 +55,19 @@ module.exports = {
       })
     }
 
-    //   ), (err, result) => {
-    //   if (err) {
-    //     //next(err)
-    //      console.log('error returned from get_Product model to getProduct controller: ', err.stack);
-    //      res.status(501);
-    //   } else {
-    //     console.log('result returned from get_Product model to getProduct controller: ', result);
-    //     res.send(result);
-    //   }
-    // });
-
-
-
-  // SEE NOTE RE get_product model working except for HTTP heaaders error. below is the controller I had running with that model.
-
-  // getProduct: function (req, res, next) {
-  //   console.log('req: ', req);
-  //   console.log('query id: ', req.params);
-  //   const { product_id } = req.params;
-  //   models.get_Product(product_id, (err, result) => {
-  //     if (err) {
-  //       //next(err)
-  //        console.log('error returned from get_Product model to getProduct controller: ', err.stack);
-  //        res.status(501);
-  //     } else {
-  //       console.log('result returned from get_Product model to getProduct controller: ', result);
-  //       res.send(result);
-  //     }
-  //   });
-  //   next();
-
-    //next(pool);
-    // db.done();
-
-
-    // could handle in next function above - controllers is now kind of acting as my transaction
-    // except it's not atomic - actually it would be if returning final result
-
-     // send client or err back to next an
-    // next(sameClient);
-}
+    // In controllers should I be parsing the errors in controllers and either resolving them or returning different error messages based on the error?
+     // How would I go about doing that?
+   // Should I include next as a third parameter here if I want to query the same client?
 
 
 
 
-//   poov
-//   .connect()
-//   .then(client => {
-//     console.log('client in getProducts controller: ', client);
-//     return client
-//       .query(query)
-//       .then(res => {
-//         client.release()
-//         console.log(res.rows[0])
-//       })
-//       .catch(err => {
-//         client.release()
-//         console.log(err.stack)
-//       })
-//   })
-//   .catch(err => {
-//     if err. === kill
-//     console.log('error connecting pool in getProduct controller')
-//     pool.end()
-//     console.log(err.stack)
-//   })
 
 
 
 
-// //psql -U postgres
-// //'\c database_name;
-
-// // add t
-
-//  //const queryString = models.getProducts_statement;
-//   const queryParams = [product_id];
-//   const queryName =  query_product;
-//   const queryObj = {};
-//   queryObj.text = queryString;
-//   queryObj.values = queryParams
-//   queryObj.name = queryName;
-//   db.clientQuery(queryObj, (err, result) => {
-//     if (err) {
-//       resolve('error getting product info for product id ')
-//       console.log('error killed command: ', err, 'parameters: ', queryObj);
-//       console.log('error making client query on client :', err.stack);
-//       res.status(500).send();
-//       // resolve(err);
-//       client.release();
-//     }
-//     console.log('result from clientQuery in controllers: ', result.rows)
-//     res.send(result.rows[0]);
-//     client.release();
-//     next();
-//   });
-//   pool.end();
-//   return client;
 
 
-    // want to make get_product model prolly perform all queries needed to get all data that needs to be updated when product_id changes
-    // and can split up the data here and cache the result data not requested yet
-    // so would want 2 or 3 functions runniung asychronously (simulanesouly)
-      // each function would be an async function that awaits a db query
-      // and then either caches or sends the result
-        // so maybe put each function in its own try block, so already have the result data correctly formatted and can have more precision in error handling
-        // also since you'll need to checkout multiple clients for the db queries, can just query.pool, but would have to then try and figure out how to return result data from diff clients in a pool to diff places
-        // but would be no diff in performance since a pool query doesn't return until all the clients are returned
-
-    // do I also want to cache product_info?
-
-//   if (next) {
-//     const nextQuery = models.get_styles
-//     const nextParams = [product_id]
-//     try {
-//       const { rows : stylesRows } = await postgres.query(models.get_styles, [product_id])
-//     }
-//     (req, res) => {
-//       if (error) {
-//         next(error)
-//       }
-//       next(null, result)
-//     }
-//   }
-//   if (error) {
-//     next
-//   }
-//   try {
-//     next() const { rows } = await postgres.query(models.get_styles, [product_id])
-//     // cache rows[0]
-//   } // handle error caching styles
-
-//   .next(
-// }
-
-// export.getStyles = async (req, res, next) => {
-//   try {
-//     const product_id = req.params.id
-//     // instead of doing the below functions, return the requested info from the cache
-//     // could maybe sommehow notify when caching complete and not begin retrieving the cached data until the notification/mark has happened
-//     const { rows } = await postgres.query(models.get_styles), [product_id]
-//     res.send(rows[0])
-//   } res.status(201).send(error)
-// }
-
-// not sure this use of next is correct
 
 
 
